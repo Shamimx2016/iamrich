b2a0f247a322a6862490ff80b9e2b5df846b555f
Shamimx2016 <shamimbd621@gmail.com>
2022-12-01 22:56:01 +0600

    new

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+          title: Text('i am rich'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
